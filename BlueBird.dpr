7dc3855169c0e05f5b97ae01b875afe10fe28dd6
ki-swan <kic.kneip@gmail.com>
2024-02-21 22:37:28 -0300

    main ajustes

--- BlueBird.dpr
+++ BlueBird.dpr
@@ -6,13 +6,13 @@ program BlueBird;
 
 uses
   System.SysUtils,
-  Bluebird.Interfaces in 'Src\Entity\Bluebird.Interfaces.pas',
-  Bluebird.Cliente in 'Src\Entity\Bluebird.Cliente.pas',
-  Bluebird.Types in 'Src\Entity\Bluebird.Types.pas';
+  Bluebird.Cliente in 'Src\Clientes\Bluebird.Cliente.pas',
+  Bluebird.Clientes.Interfaces in 'Src\Clientes\Bluebird.Clientes.Interfaces.pas',
+  Bluebird.Clientes.types in 'Src\Clientes\Bluebird.Clientes.types.pas';
 
 begin
   try
-    TBluebirdClienteAssinado.New.cliente
+
   except
     on E: Exception do
       Writeln(E.ClassName, ': ', E.Message);
